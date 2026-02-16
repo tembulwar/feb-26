179b0ea27b38b1ed039814467301975e9b321187
tembulwar <tembulwarvip710@gmail.com>
2026-02-16 15:02:41 +0530

    Day-1 Content

---
+++ Demo.java
@@ -0,0 +1,30 @@
+class Demo{
+	//String $s;
+	//String firstName;
+	//String firstName; error
+
+	// String String;
+
+	// int int; error
+
+	public static void main(String args[]){
+		int a;
+		a=10;
+		int a1 = 10;
+		System.out.println("Welcome to C-DAC Mumbai !!");
+		System.out.println(a);
+
+	}
+
+}
+
+
+//System.out.println("Welcome to C-DAC Mumbai !!");
+
+
+
+class demo1{
+
+
+
+}
---
+++ Test.java
@@ -0,0 +1,30 @@
+class Test{
+	
+
+	public static void main(String args[]){
+		int a;
+		a=10;
+		int a1 = 10;
+
+		char c1 = '1';
+
+		float f = 10.0f;
+
+
+		long l = 1000L;
+	
+		int i = l;
+		
+		System.out.println("Welcome to C-DAC Mumbai !!");
+		System.out.println(a);
+		System.out.println(c1);
+		System.out.println(f);
+		System.out.println(l);
+		System.out.println(i);
+
+
+
+
+	}
+
+}
